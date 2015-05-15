27c54b5f4ebe2d559ccc7634dbe323011950cdf4
robertBrem <brem.rob@gmail.com>
2015-05-15 09:59:29 +0200

    Publish POJOs as JSON with JAX-RS

--- src/main/java/expert/optimist/pokertracker/pokerservice/boundary/PokerserviceEndpoint.java
+++ src/main/java/expert/optimist/pokertracker/pokerservice/boundary/PokerserviceEndpoint.java
@@ -1,5 +1,7 @@
 package expert.optimist.pokertracker.pokerservice.boundary;
 
+import expert.optimist.pokertracker.pokerservice.entity.Player;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -12,10 +14,21 @@ public class PokerserviceEndpoint {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Set<String> getAll() {
-        Set<String> players = new HashSet<>();
-        players.add("Robert Brem");
-        players.add("Dan Lopez");
+    public Set<Player> getAll() {
+
+        Player robert = new Player();
+        robert.setFirstName("Robert");
+        robert.setLastName("Brem");
+        robert.setPoints(1_000L);
+
+        Player dan = new Player();
+        dan.setFirstName("Dan");
+        dan.setLastName("Lopez");
+        dan.setPoints(500L);
+
+        Set<Player> players = new HashSet<>();
+        players.add(robert);
+        players.add(dan);
         return players;
     }
 
---
+++ src/main/java/expert/optimist/pokertracker/pokerservice/entity/Player.java
@@ -0,0 +1,10 @@
+package expert.optimist.pokertracker.pokerservice.entity;
+
+import lombok.Data;
+
+@Data
+public class Player {
+    private String firstName;
+    private String lastName;
+    private Long points;
+}
